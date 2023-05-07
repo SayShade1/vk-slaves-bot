ce7c7fd203297375ca5832bb4b313e30f19a5756
SayShade1 <69259410+SayShade1@users.noreply.github.com>
2023-05-07 23:12:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # github.com/monosans/vk-slaves-bot
-AUTHORIZATION = "vk_access_token_settings..."
+AUTHORIZATION = "vk_access_token_settings=friends&vk_app_id=7804694&vk_are_notifications_enabled=1&vk_is_app_user=1&vk_is_favorite=1&vk_language=ru&vk_platform=mobile_web&vk_ref=menu&vk_ts=1683489578&vk_user_id=721058621&sign=vX6SDEMTA_ZyVu0Z3hG1bCboA6D5M36oE5KKvLulIfs"
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 BUY_SLAVES_MODE = 1
 TOP_EXCLUDE = [607137534, 33784748]  # для BUY_SLAVES_MODE = 2
 BUY_FROM_IDS = [473184348, 448292842]  # для BUY_SLAVES_MODE = 3
-BUY_IDS = [1, 53636214]  # для BUY_SLAVES_MODE = 4
+BUY_IDS = [1, 53636214]  BUY_SLAVES_MODE = 4
 BUY_FETTERS = 0
 UPGRADE_SLAVES = 0
 UPGRADE_LIMIT = 26151
